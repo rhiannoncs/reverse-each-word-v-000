0376ef92e483ba7eefb42b9d7cabdfc0d94f9071
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-01-25 03:48:25 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,5 @@
 def reverse_each_word(string)
   string_array = string.split
-  reversed_array = []
-  string_array.each do |word|
-    reversed_array << word.reverse
-  end
+  reversed_array = string_array.collect{|word| word.reverse}
   reversed_array.join(" ")
 end
